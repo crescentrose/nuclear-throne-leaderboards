95f010ca0d2b77682ce4e4e3dd3f2efa79fbf61d
Darwin <tko.netko@gmail.com>
2015-01-13 00:02:48 +0100

    added more stuff to the faq

--- templates/about.php
+++ templates/about.php
@@ -17,6 +17,8 @@
   <p>A: Yes, don't play the game. <i>(badum-tss)</i> All of the data on this page is already public via Steam APIs - there's no black magic going on. However there might be a opt-out feature in the near future.</p>
   <b>Q: I got marked as a suspected hacker - what did I do, what do I do?</b>
   <p>A: All marks are manual - this means I saw your score, thought to myself "Yep, this guy definitely got 100 000 kills 25 minutes into the daily" and checked a small box to show that to the world. There are no perfect methods, though, so if you can display, on video or with an otherwise verifiable source, a similar skill level without using any hacks, then I will remove the mark. </p>
+  <b>Q: What does [private] mean?</b>
+  <p>A: That user did not yet set up their Steam Community profile, so there's no data on them.</p>
   <b>Q: Can I pimp my profile?</b>
   <p>A: Profiles are in a very basic state as of now - expect lots of upgrades on that front. If there's enough interest you'll be able to edit them.</p>
   <b>Q: I've found a bug or have an idea - who do I yell at?</b>
