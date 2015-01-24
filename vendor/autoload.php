cc7ccb500c78ea7c2733a97eaf20043a306e7dad
Darwin <tko.netko@gmail.com>
2015-01-24 19:05:46 +0100

    Updated LightOpenID

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit574d9dcbcab8eb6605cffeed8c4535fa::getLoader();
+return ComposerAutoloaderInit8b0c96b32950eddd43f120396816a59d::getLoader();
