da2a033613acf4d988de444ee4aa3e10d1831c63
Ivan <i542@live.com>
2015-04-16 18:44:11 +0200

    Moved config to separate folder

--- models/application.php
+++ models/application.php
@@ -4,7 +4,7 @@ class Application {
 	private $database_password, $database_host, $database_username, $database_name;
 
 	public static function connect() {
-		$config_file = file_get_contents("config.json");
+		$config_file = file_get_contents("config/config.json");
 		$config = json_decode($config_file, true);
 		return new PDO('mysql:host='. $config["Database"]["host"] .';dbname='. $config["Database"]["name"] . ';charset=utf8', $config["Database"]["username"], $config["Database"]["password"], array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 	}
