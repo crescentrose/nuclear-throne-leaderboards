caaedbf56ce45e200f5683496aae698b6faa278d
Ivan <i542@live.com>
2015-06-21 19:29:19 +0200

    Added a twitter box

--- templates/index.php
+++ templates/index.php
@@ -265,6 +265,8 @@
             </div>
     </div>
 {% endif %}
+  <a class="twitter-timeline" href="https://twitter.com/thronebutt" data-widget-id="612672701031915520">Tweets by @thronebutt</a>
+  <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
     </div>
     </div>
   </div>
